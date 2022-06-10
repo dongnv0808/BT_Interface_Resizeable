3086091631117839d905fd59df44ea957ad8564d
dongnv0808 <yamaraja0808@gmail.com>
2022-06-10 15:18:50 +0700

    update

--- dist/main.js
+++ dist/main.js
@@ -3,16 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Circle_1 = require("./Circle");
 const Rectangle_1 = require("./Rectangle");
 const Square_1 = require("./Square");
-let shape = [];
-shape[0] = new Circle_1.Circle("Hinh tron", 3);
-console.log(`Dien tich hinh tron = ${shape[0].getArea()}`);
-shape[1] = new Rectangle_1.Rectangle("Hinh chu nhat", 10, 5);
-console.log(`Dien tich hinh chu nhat = ${shape[1].getArea()}`);
-shape[2] = new Square_1.Square("Hinh vuong", 5);
-console.log(`Dien tich hinh vuong = ${shape[2].getArea()}`);
-let percentRandom = (Math.floor(Math.random() * 100) + 1) / 100;
-let percentRandom1 = (Math.floor(Math.random() * 100) + 1) / 100;
-let percentRandom2 = (Math.floor(Math.random() * 100) + 1) / 100;
-console.log(`Dien tich hinh tron tang ${percentRandom * 100}% = ${shape[0].getArea() + (shape[0].getArea() * percentRandom)}`);
-console.log(`Dien tich hinh chu nhat tang ${percentRandom1 * 100}% = ${shape[1].getArea() + (shape[1].getArea() * percentRandom1)}`);
-console.log(`Dien tich hinh vuong tang ${percentRandom2 * 100}% = ${shape[2].getArea() + (shape[2].getArea() * percentRandom2)}`);
+let shapes = [];
+shapes[0] = new Circle_1.Circle("Hinh tron", 3);
+// console.log(`Dien tich hinh tron = ${shape[0].getArea()}`);
+shapes[1] = new Rectangle_1.Rectangle("Hinh chu nhat", 10, 5);
+// console.log(`Dien tich hinh chu nhat = ${shape[1].getArea()}`);
+shapes[2] = new Square_1.Square("Hinh vuong", 5);
+// console.log(`Dien tich hinh vuong = ${shape[2].getArea()}`);
+// let percentRandomCircle = (Math.floor(Math.random() * 100)+1)/100
+// let percentRandomRectangle = (Math.floor(Math.random() * 100)+1)/100
+// let percentRandomSquare = (Math.floor(Math.random() * 100)+1)/100
+// console.log(`Dien tich hinh tron tang ${percentRandomCircle * 100}% = ${shapes[0].getArea() + (shapes[0].getArea() * percentRandomCircle)}`);
+// console.log(`Dien tich hinh chu nhat tang ${percentRandomRectangle * 100}% = ${shapes[1].getArea() + (shapes[1].getArea() * percentRandomRectangle)}`);
+// console.log(`Dien tich hinh vuong tang ${percentRandomSquare * 100}% = ${shapes[2].getArea() + (shapes[2].getArea() * percentRandomSquare)}`);
+for (let shape of shapes) {
+    let percentRandom = (Math.floor(Math.random() * 100) + 1) / 100;
+    console.log(`Dien tich truoc khi tang ${shape.getArea()} | Dien tich sau khi tang ${percentRandom * 100}% = ${shape.getArea() + (shape.getArea() * percentRandom)}`);
+}
--- main.ts
+++ main.ts
@@ -1,17 +1,22 @@
 import { Circle } from "./Circle";
 import { Rectangle } from "./Rectangle";
+import { Shape } from "./Shape";
 import { Square } from "./Square";
 
-let shape = [];
-shape[0] = new Circle("Hinh tron", 3);
-console.log(`Dien tich hinh tron = ${shape[0].getArea()}`);
-shape[1] = new Rectangle("Hinh chu nhat", 10, 5);
-console.log(`Dien tich hinh chu nhat = ${shape[1].getArea()}`);
-shape[2] = new Square("Hinh vuong", 5);
-console.log(`Dien tich hinh vuong = ${shape[2].getArea()}`);
-let percentRandomCircle = (Math.floor(Math.random() * 100)+1)/100
-let percentRandomRectangle = (Math.floor(Math.random() * 100)+1)/100
-let percentRandomSquare = (Math.floor(Math.random() * 100)+1)/100
-console.log(`Dien tich hinh tron tang ${percentRandomCircle * 100}% = ${shape[0].getArea() + (shape[0].getArea() * percentRandomCircle)}`);
-console.log(`Dien tich hinh chu nhat tang ${percentRandomRectangle * 100}% = ${shape[1].getArea() + (shape[1].getArea() * percentRandomRectangle)}`);
-console.log(`Dien tich hinh vuong tang ${percentRandomSquare * 100}% = ${shape[2].getArea() + (shape[2].getArea() * percentRandomSquare)}`);
+let shapes: Shape[] = [];
+shapes[0] = new Circle("Hinh tron", 3);
+// console.log(`Dien tich hinh tron = ${shape[0].getArea()}`);
+shapes[1] = new Rectangle("Hinh chu nhat", 10, 5);
+// console.log(`Dien tich hinh chu nhat = ${shape[1].getArea()}`);
+shapes[2] = new Square("Hinh vuong", 5);
+// console.log(`Dien tich hinh vuong = ${shape[2].getArea()}`);
+// let percentRandomCircle = (Math.floor(Math.random() * 100)+1)/100
+// let percentRandomRectangle = (Math.floor(Math.random() * 100)+1)/100
+// let percentRandomSquare = (Math.floor(Math.random() * 100)+1)/100
+// console.log(`Dien tich hinh tron tang ${percentRandomCircle * 100}% = ${shapes[0].getArea() + (shapes[0].getArea() * percentRandomCircle)}`);
+// console.log(`Dien tich hinh chu nhat tang ${percentRandomRectangle * 100}% = ${shapes[1].getArea() + (shapes[1].getArea() * percentRandomRectangle)}`);
+// console.log(`Dien tich hinh vuong tang ${percentRandomSquare * 100}% = ${shapes[2].getArea() + (shapes[2].getArea() * percentRandomSquare)}`);
+for(let shape of shapes){
+    let percentRandom = (Math.floor(Math.random() * 100)+1)/100;
+    console.log(`Dien tich truoc khi tang ${shape.getArea()} | Dien tich sau khi tang ${percentRandom * 100}% = ${shape.getArea() + (shape.getArea() * percentRandom)}`);
+}
